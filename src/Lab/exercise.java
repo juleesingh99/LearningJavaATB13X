b8f56d6e066b54484ef0aae2cb744087f34e8b91
juleesingh99 <julee99@gmail.com>
2025-06-06 08:55:08 +0530

    commit file1 update

--- src/Lab/exercise.java
+++ src/Lab/exercise.java
@@ -2,6 +2,7 @@ package Lab;
 
 public class exercise {
     public static void main(String[] args) {
+        System.out.println("first git Push"); System.out.println("first git Push");
         System.out.println("first git Push");
     }
 }
